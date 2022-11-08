53324e3516cb97116ab73daa4c4a424b2fd6c0ac
nickonyi <nickoluoch1130@gmail.com>
2022-11-08 11:13:18 +0300

    responsive nav bar menu

--- css/responsive-navbar-menu/script.js
+++ css/responsive-navbar-menu/script.js
@@ -1,1 +1,23 @@
-console.log("hello world");
+function showMenu(toggleId, navId) {
+    const toggle = document.getElementById(toggleId);
+    const nav = document.getElementById(navId);
+
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show');
+            toggle.classList.toggle('bx-x');
+        });
+    }
+}
+
+showMenu('header-toggle', 'nav-menu');
+
+const navLink = document.querySelectorAll('.nav-link');
+
+function linkAction() {
+    navLink.forEach(n => n.classList.remove('active'));
+    this.classList.add('active');
+}
+
+navLink.forEach(n => n.addEventListener("click", linkAction));
