b0e530c4912ab6e3cf92df1db70ec2d703a5e5a8
nickonyi <nickoluoch1130@gmail.com>
2022-12-05 12:16:14 +0300

    book constructor

--- javascript/object-object-constructors/book-exercise/index.js
+++ javascript/object-object-constructors/book-exercise/index.js
@@ -1,1 +1,11 @@
-console.log("2.40");
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        console.log(` The ${this.title} by ${this.author}, ${this.pages}, ${this.read}`);
+    }
+}
+const silkyRoad = new Book("silky Road", "Baren Davis", 456, "Not yet read");
+silkyRoad.info();
