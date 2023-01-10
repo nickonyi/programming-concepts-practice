211b3bcbf306416f8750457945f06d658b5713b4
nickonyi <nickoluoch1130@gmail.com>
2023-01-10 16:26:45 +0300

    work

--- javascript/object-object-constructors/constructor/index.js
+++ javascript/object-object-constructors/constructor/index.js
@@ -5,19 +5,65 @@ let proto = {
 function C() {
     this.cprop = "cprop";
 }
-C.prototype = proto;
+//C.prototype = proto;
+//
+//function F() {
+//    this.fprop = "fprop";
+//}
+//F.prototype = proto;
+//
+//let f = new F();
+//let c = new C();
+//
+//
+//console.log(f.protocrop);
+//console.log(f.fprop);
+//console.log(f.cprop);
+//console.log(f instanceof F);
+//console.log(c.constructor === Object);
 
-function F() {
-    this.fprop = "fprop";
+//function myObject(data) {
+//    let obj = Object.create(myObject.proto);
+//    obj.data = data;
+//    return obj;
+//}
+//
+//myObject.proto = {
+//    getData: function() {
+//        return this.data;
+//    }
+//}
+//
+//let obij = myObject("wakawakaeeeeb h");
+//console.log(obij.data);
+
+//function personFactory(name, age) {
+//    const sayHello = () => console.log(name + " " + age + " hello");
+//    return { name, age, sayHello };
+//}
+//
+//const jeff = personFactory("jeff", 27);
+//console.table(jeff);
+//jeff.sayHello();
+
+//const name = "Maynard";
+//const color = "red";
+//const number = 34;
+//const food = "rice";
+//
+//// logging all of these variables might be a useful thing to do,
+//// but doing it like this can be somewhat confusing.
+//console.log(name, color, number, food); // Maynard red 34 rice
+//
+//// if you simply turn them into an object with brackets,
+//// the output is much easier to decipher:
+//console.table({ name, color, number, food });
+//// { name: 'Maynard', color: 'red', number: 34, food: 'rice' }
+
+
+
+function foo() {
+    a = 5;
 }
-F.prototype = proto;
-
-let f = new F();
-let c = new C();
-
-
-console.log(f.protocrop);
-console.log(f.fprop);
-console.log(f.cprop);
-console.log(f instanceof F);
-console.log(c.constructor === C);
+foo();
+console.log(a);
