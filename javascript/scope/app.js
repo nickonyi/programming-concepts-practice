360a6b013f2cfe45959af5cf376f9364464a54b4
nickonyi <nickoluoch1130@gmail.com>
2023-01-11 10:57:53 +0300

    scope tings

--- javascript/scope/app.js
+++ javascript/scope/app.js
@@ -5,3 +5,30 @@
 //    console.log(`You are ${dogYears} dog years old!`);
 //
 //}
+
+//const factoryFunction = String => {
+//    const capitalizeString = () => String.toUpperCase();
+//    const printString = () => console.log(`----${capitalizeString()}------`);
+//    return { printString, capitalizeString };
+//}
+//
+//const taco = factoryFunction("taco bell sponser me");
+//
+//taco.printString();
+//
+//console.log(taco.capitalizeString())
+
+const counterCreator = () => {
+    let count = 0;
+    return () => {
+        console.log(count);
+        count++;
+    }
+}
+
+let count = counterCreator();
+count();
+count();
+count();
+count();
+count();
