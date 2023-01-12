2dead332098b421c1d24d64cc17c8b1c15f53058
nickonyi <nickoluoch1130@gmail.com>
2023-01-12 11:11:07 +0300

    scope tings

--- javascript/scope/game.js
+++ javascript/scope/game.js
@@ -1,34 +1,50 @@
-const Player = (name, level) => {
-    let health = level * 2;
-    const getLevel = () => level;
-    const getName = () => name;
-    const die = () => {
-        console.log("Uh oh You are wasted my friend");
-    }
-    const damage = x => {
-        health -= x;
-        if (health <= 0) {
-            die();
-        }
-    }
+//const Player = (name, level) => {
+//    let health = level * 2;
+//    const getLevel = () => level;
+//    const getName = () => name;
+//    const die = () => {
+//        console.log("Uh oh You are wasted my friend");
+//    }
+//    const damage = x => {
+//        health -= x;
+//        if (health <= 0) {
+//            die();
+//        }
+//    }
+//
+//    const attack = enemy => {
+//        if (level < enemy.getLevel()) {
+//            damage(1);
+//            console.log(`${enemy.getName()} has damaged ${name}`);
+//        } else if (level >= enemy.getLevel()) {
+//            enemy.damage(1);
+//            console.log(`${name} has damaged ${enemy.getName()}`);
+//        }
+//    }
+//    return { attack, damage, getName, getLevel }
+//}
+//
+//const rambo = Player("Rambo", 4);
+//const badGuy = Player("Billie", 5);
+//
+//rambo.attack(badGuy);
+//
+//
+//
+//console.log(rambo.health);
 
-    const attack = enemy => {
-        if (level < enemy.getLevel()) {
-            damage(1);
-            console.log(`${enemy.getName()} has damaged ${name}`);
-        } else if (level >= enemy.getLevel()) {
-            enemy.damage(1);
-            console.log(`${name} has damaged ${enemy.getName()}`);
-        }
-    }
-    return { attack, damage, getName, getLevel }
+function Boy() {
+
+}
+Boy.prototype.getSport = function() {
+    console.log("basketball");
+};
+
+function Girl(name) {
+    this.name = name;
 }
 
-const rambo = Player("Rambo", 4);
-const badGuy = Player("Billie", 5);
+Girl.prototype = Object.create(Boy.prototype);
 
-rambo.attack(badGuy);
-
-
-
-console.log(rambo.health);
+const cynthia = new Girl("Cynthia");
+cynthia.getSport();
--- javascript/scope/inhertance.js
+++ javascript/scope/inhertance.js
@@ -1,14 +1,20 @@
-const person = (name) => {
-    const sayName = () => console.log(`my name is chicki chicki ${name}`);
-    return { sayName };
-}
+//const person = (name) => {
+//    const sayName = () => console.log(`my name is chicki chicki ${name}`);
+//    return { sayName };
+//}
+//
+//const nerd = (name) => {
+//    const { sayName } = person(name);
+//    const doNerdyStaff = () => console.log("Nerd staff");
+//    return { sayName, doNerdyStaff };
+//}
+//
+//const nick = nerd("Slim shady");
+//
+//
 
-const nerd = (name) => {
-    const { sayName } = person(name);
-    const doNerdyStaff = () => console.log("Nerd staff");
-    return { sayName, doNerdyStaff };
-}
+const target = { a: 1, b: 2 };
+const source = { c: 3, d: 4 };
 
-const nick = nerd("Slim shady");
-
-nick.sayName();
+const returnObj = Object.assign(source, target);
+console.table(returnObj);
