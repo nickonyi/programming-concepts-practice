84fda7730f1a3aec7a4307295fe6bca0f8ee9239
nickonyi <nickoluoch1130@gmail.com>
2023-02-03 11:58:51 +0300

    min max

--- javascript/min-max-algo/index.js
+++ javascript/min-max-algo/index.js
@@ -1,3 +1,37 @@
 var origBoard = ["O", 1, "X", "X", 4, "X", 6, "O", "O"];
 var huplayer = "X";
 var aiplayer = "O";
+var score = 0;
+
+function emptyIndexes(board) {
+    return board.filter(s => s != "O" && s != "X");
+}
+
+function winning(board, player) {
+    if (
+        (board[0] == player && board[1] == player && board[2] == player) ||
+        (board[3] == player && board[4] == player && board[5] == player) ||
+        (board[6] == player && board[7] == player && board[8] == player) ||
+        (board[0] == player && board[3] == player && board[6] == player) ||
+        (board[1] == player && board[4] == player && board[7] == player) ||
+        (board[2] == player && board[5] == player && board[8] == player) ||
+        (board[0] == player && board[4] == player && board[8] == player) ||
+        (board[2] == player && board[4] == player && board[6] == player)
+    ) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function minmax(newBoard, player) {
+    let availSpots = emptyIndexes(newBoard);
+
+    if (winning(newBoard, huplayer)) {
+        return { score: -10 }
+    } else if (winning(newBoard, aiplayer)) {
+        return { score: 10 }
+    } else if (availSpots.length === 0) {
+        return { score: 0 }
+    }
+}
